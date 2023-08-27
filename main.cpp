4098da26e0af7baaa7e2737592e47d6b0d89a471
artem <artemmartus2012@gmail.com>
2023-08-27 09:35:35 +0000

    etrgfsgsretf

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 //
-// async_udp_echo_server.cpp
-// ~~~~~~~~~~~~~~~~~~~~~~~~~
+// http_client.cpp
+// ~~~~~~~~~~~~~~~
 //
 // Copyright (c) 2003-2023 Christopher M. Kohlhoff (chris at kohlhoff dot com)
 //
@@ -8,75 +8,83 @@
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 //
 
-#include <cstdlib>
 #include <iostream>
-#include <boost/asio/ts/buffer.hpp>
+#include <istream>
+#include <ostream>
+#include <string>
 #include <boost/asio/ts/internet.hpp>
 
-using boost::asio::ip::udp;
-
-class server
-{
-public:
-  server(boost::asio::io_context& io_context, short port)
-    : socket_(io_context, udp::endpoint(udp::v4(), port))
-  {
-    do_receive();
-  }
-
-  void do_receive()
-  {
-    socket_.async_receive_from(
-        boost::asio::buffer(data_, max_length), sender_endpoint_,
-        [this](boost::system::error_code ec, std::size_t bytes_recvd)
-        {
-          if (!ec && bytes_recvd > 0)
-          {
-            do_send(bytes_recvd);
-          }
-          else
-          {
-            do_receive();
-          }
-        });
-  }
-
-  void do_send(std::size_t length)
-  {
-    socket_.async_send_to(
-        boost::asio::buffer(data_, length), sender_endpoint_,
-        [this](boost::system::error_code /*ec*/, std::size_t /*bytes_sent*/)
-        {
-          do_receive();
-        });
-  }
-
-private:
-  udp::socket socket_;
-  udp::endpoint sender_endpoint_;
-  enum { max_length = 1024 };
-  char data_[max_length];
-};
+using boost::asio::ip::tcp;
 
 int main(int argc, char* argv[])
 {
   try
   {
-    if (argc != 2)
+    if (argc != 3)
     {
-      std::cerr << "Usage: async_udp_echo_server <port>\n";
+      std::cout << "Usage: http_client <server> <path>\n";
+      std::cout << "Example:\n";
+      std::cout << "  http_client www.boost.org /LICENSE_1_0.txt\n";
       return 1;
     }
 
-    boost::asio::io_context io_context;
+    boost::asio::ip::tcp::iostream s;
 
-    server s(io_context, std::atoi(argv[1]));
+    // The entire sequence of I/O operations must complete within 60 seconds.
+    // If an expiry occurs, the socket is automatically closed and the stream
+    // becomes bad.
+    s.expires_after(std::chrono::seconds(60));
 
-    io_context.run();
+    // Establish a connection to the server.
+    s.connect(argv[1], "http");
+    if (!s)
+    {
+      std::cout << "Unable to connect: " << s.error().message() << "\n";
+      return 1;
+    }
+
+    // Send the request. We specify the "Connection: close" header so that the
+    // server will close the socket after transmitting the response. This will
+    // allow us to treat all data up until the EOF as the content.
+    s << "GET " << argv[2] << " HTTP/1.0\r\n";
+    s << "Host: " << argv[1] << "\r\n";
+    s << "Accept: */*\r\n";
+    s << "Connection: close\r\n\r\n";
+
+    // By default, the stream is tied with itself. This means that the stream
+    // automatically flush the buffered output before attempting a read. It is
+    // not necessary not explicitly flush the stream at this point.
+
+    // Check that response is OK.
+    std::string http_version;
+    s >> http_version;
+    unsigned int status_code;
+    s >> status_code;
+    std::string status_message;
+    std::getline(s, status_message);
+    if (!s || http_version.substr(0, 5) != "HTTP/")
+    {
+      std::cout << "Invalid response\n";
+      return 1;
+    }
+    if (status_code != 200)
+    {
+      std::cout << "Response returned with status code " << status_code << "\n";
+      return 1;
+    }
+
+    // Process the response headers, which are terminated by a blank line.
+    std::string header;
+    while (std::getline(s, header) && header != "\r")
+      std::cout << header << "\n";
+    std::cout << "\n";
+
+    // Write the remaining data to output.
+    std::cout << s.rdbuf();
   }
   catch (std::exception& e)
   {
-    std::cerr << "Exception: " << e.what() << "\n";
+    std::cout << "Exception: " << e.what() << "\n";
   }
 
   return 0;
